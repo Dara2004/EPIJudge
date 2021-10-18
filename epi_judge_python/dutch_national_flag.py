2d4011a452a5cef8e7ddd58414f15627ec50a810
Dara Nguyen <duongnt1204@gmail.com>
2021-10-17 17:16:06 -0700

    Update dutch_national_flag.py

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -7,11 +7,33 @@ from test_framework.test_utils import enable_executor_hook
 
 RED, WHITE, BLUE = range(3)
 
+### first solution ###
+# Time: O(2N), space: O(1)
+# def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
+#     smaller, pivot, larger = 0, A[pivot_index], len(A) - 1
+#     for i in range(0, len(A)):
+#         if A[i] < pivot:
+#             A[i], A[smaller] = A[smaller], A[i] #swap array elements
+#             smaller += 1
+#     for i in reversed(range(0, len(A))):
+#         if A[i] > pivot:
+#             A[i], A[larger] = A[larger], A[i]
+#             larger -= 1
 
+### optimal solution ###
+# Time: O(N), space: O(1)
 def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
-
+    s, e, l, pivot = 0, 0, len(A) - 1, A[pivot_index] #smaller, equal, larger
+    while e <= l:
+        if A[e] < pivot:
+            A[e], A[s] = A[s], A[e]
+            e += 1
+            s += 1
+        elif A[e] == pivot:
+            e += 1
+        else:
+            A[e], A[l] = A[l], A[e]
+            l -= 1
 
 @enable_executor_hook
 def dutch_flag_partition_wrapper(executor, A, pivot_idx):
