dc9e3f74aecb84d50cd22d9124edc8fac4a1fbd8
Dara Nguyen <duongnt1204@gmail.com>
2021-10-18 21:45:53 -0700

    solved advanced_by_offset

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -1,11 +1,35 @@
+import sys
 from typing import List
 
 from test_framework import generic_test
 
 
 def can_reach_end(A: List[int]) -> bool:
-    # TODO - you fill in here.
-    return True
+    memo = [-1 for i in range(len(A))]
+    return can_reach_end_helper(A, 0, memo)
+
+def can_reach_end_helper(A: List[int], currIdx: int, memo: List[int]) -> bool:
+    # print("currIdx: ", currIdx)
+    if memo[currIdx] != -1:
+        return memo[currIdx]
+
+    maxStep = A[currIdx]
+    # print("maxStep: ", maxStep)
+
+    if currIdx >= len(A) - 1:
+        return True
+
+    if currIdx < len(A) - 1 and maxStep == 0:
+        return False
+
+    for i in range(1, maxStep+1):
+        canReachEndFromCurrIdx = can_reach_end_helper(A, currIdx + i, memo)
+        if (canReachEndFromCurrIdx):
+            memo[currIdx] = True
+            return True
+
+    memo[currIdx] = False
+    return False
 
 
 if __name__ == '__main__':
--- problem_mapping.js
+++ problem_mapping.js
@@ -236,7 +236,7 @@ problem_mapping = {
                 "total": 2004
             },
             "Python: advance_by_offsets.py": {
-                "passed": 0,
+                "passed": 1,
                 "total": 2004
             }
         },
