341d31d0c04588705eac7912c25ffb235c89337b
Dara Nguyen <duongnt1204@gmail.com>
2021-10-18 21:55:51 -0700

    advance_by_offset optimal

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -5,31 +5,35 @@ from test_framework import generic_test
 
 
 def can_reach_end(A: List[int]) -> bool:
-    memo = [-1 for i in range(len(A))]
-    return can_reach_end_helper(A, 0, memo)
+    can_reach = 0
+    for i in range(0, len(A)):
+        if i <= can_reach:
+            can_reach = max(can_reach, A[i] + i)
+    return can_reach >= len(A) - 1
 
-def can_reach_end_helper(A: List[int], currIdx: int, memo: List[int]) -> bool:
-    # print("currIdx: ", currIdx)
-    if memo[currIdx] != -1:
-        return memo[currIdx]
 
-    maxStep = A[currIdx]
-    # print("maxStep: ", maxStep)
-
-    if currIdx >= len(A) - 1:
-        return True
-
-    if currIdx < len(A) - 1 and maxStep == 0:
-        return False
-
-    for i in range(1, maxStep+1):
-        canReachEndFromCurrIdx = can_reach_end_helper(A, currIdx + i, memo)
-        if (canReachEndFromCurrIdx):
-            memo[currIdx] = True
-            return True
-
-    memo[currIdx] = False
-    return False
+# def can_reach_end_helper(A: List[int], currIdx: int, memo: List[int]) -> bool:
+#     # print("currIdx: ", currIdx)
+#     if memo[currIdx] != -1:
+#         return memo[currIdx]
+#
+#     maxStep = A[currIdx]
+#     # print("maxStep: ", maxStep)
+#
+#     if currIdx >= len(A) - 1:
+#         return True
+#
+#     if currIdx < len(A) - 1 and maxStep == 0:
+#         return False
+#
+#     for i in range(1, maxStep+1):
+#         canReachEndFromCurrIdx = can_reach_end_helper(A, currIdx + i, memo)
+#         if (canReachEndFromCurrIdx):
+#             memo[currIdx] = True
+#             return True
+#
+#     memo[currIdx] = False
+#     return False
 
 
 if __name__ == '__main__':
